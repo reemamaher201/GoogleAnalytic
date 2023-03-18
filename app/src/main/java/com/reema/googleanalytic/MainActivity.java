b3cfe648cec6b109dd697b20975f724d8d00901e
Reema <121347238+reemamaher201@users.noreply.github.com>
2023-03-18 09:36:07 +0200

    First commit

--- app/src/main/java/com/reema/googleanalytic/MainActivity.java
+++ app/src/main/java/com/reema/googleanalytic/MainActivity.java
@@ -1,14 +1,100 @@
 package com.reema.googleanalytic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    Button buttonEvent;
+    Calendar calendar = Calendar.getInstance();
+    int hour = calendar.get(Calendar.HOUR);
+    int minute = calendar.get(Calendar.MINUTE);
+    int second = calendar.get(Calendar.SECOND);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        buttonEvent=findViewById(R.id.btnEvent);
+        screenTrack("Home Screen");
+        buttonEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnEvent("food@1","Food Button","Button");
+            }
+        });
+
+
+    }
+    // select content
+    public void btnEvent(String id,String name,String content){
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE,content);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+    }
+    public void screenTrack(String screenName){
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, screenName);
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, "Main Activity");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle);
+    }
+
+    @Override
+    protected void onPause() {
+
+        Calendar calendar = Calendar.getInstance();
+        int hour2 = calendar.get(Calendar.HOUR);
+        int minute2 = calendar.get(Calendar.MINUTE);
+        int second2 = calendar.get(Calendar.SECOND);
+
+        int h = hour2-hour;
+        int m = minute2-minute;
+        int s = second2-second;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        HashMap<String,Object> users = new HashMap<>();
+        users.put("name","Reema");
+        users.put("hours",h);
+        users.put("minute",m);
+        users.put("seconds",s);
+
+        db.collection("users").add(users)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+        Log.e("Hours",String.valueOf(h));
+        Log.e("Minutes",String.valueOf(m));
+        Log.e("Seconds",String.valueOf(s));
+        super.onPause();
     }
 }
